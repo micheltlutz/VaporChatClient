9c0f01df39ecf358b9d6e852184ad80cdf37528f
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2019-08-27 23:16:18 -0300

    Initial Commit

---
+++ VaporChatClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VaporChatClient
+//
+//  Created by Michel Anderson Lutz Teixeira on 27/08/19.
+//  Copyright © 2019 Michel Lutz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
